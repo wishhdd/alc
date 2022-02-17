45bc2b69de9c653b6e962aa970e895c69a09823e
Kornilov Konstantin <wishhdd@gmail.com>
2022-02-17 13:49:53 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace alc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(450, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(450, 462);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "выбери лог";
+            this.openFileDialog1.Filter = "лог|*.txt";
+            this.openFileDialog1.Title = "Выберил файл клеймора";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 521);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(474, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 544);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Анализ логов Клеймора";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace alc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        List<List<string>> gor = new List<List<string>>(); //gor[0][0] - название видео карты; gor[0][1] - ее номер gpu; все остальное - значение.
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                backgroundWorker1.RunWorkerAsync(openFileDialog1.FileName) ;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //SHDocVw.InternetExplorer IE;
+            //IE = new SHDocVw.InternetExplorer();
+            //IE.Navigate("http://nnmgp.ru/");
+            //IE.Visible = true;
+            System.Diagnostics.Process.Start("http://nnmgp.ru/");
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            this.Invoke(new MethodInvoker(() => button1.Enabled = false));
+            this.Invoke(new MethodInvoker(() => textBox1.Text = "Не нерничай, считаем, смотри на прогресс бар ↓↓↓"));
+            string put = (string)e.Argument;
+            {
+                int t = 0;
+                ArrayList al = new ArrayList();
+                al.AddRange(System.IO.File.ReadAllLines(put));
+                this.Invoke(new MethodInvoker(() => progressBar1.Maximum = al.Count));
+                for (int q = 0; q < al.Count; q++)
+                {
+                    this.Invoke(new MethodInvoker(() => progressBar1.Value = q));
+
+                    if (al[q].ToString().Contains("Cards available"))
+                    {
+
+                        for (int r = 1; ; r++)
+                        {
+                            if (al[q + r].ToString().Contains("Total cards:"))
+                            {
+                                // MessageBox.Show("fuf");
+                                q = q + r;
+                                break;
+                            }
+                            else
+                            {
+                                if (al[q + r].ToString().Contains("GPU #" + t + ":"))
+                                {
+                                    //al[q+1].ToString().
+                                    int a = al[q + r].ToString().IndexOf("GPU");
+                                    int b = al[q + r].ToString().IndexOf(",");
+                                    //textBox1.Text = al[q + 1].ToString().Substring(a+8, b-a-8);
+                                    gor.Add(new List<string>());
+                                    gor[t].Add(al[q + r].ToString().Substring(a + 8, b - a - 8));   //Название видео карты
+                                    gor[t].Add(al[q + r].ToString().Substring(a, 6));   //номер gpu
+                                    gor[t].Add("");   //Средняя скорость GPU
+                                    gor[t].Add("0");   //колличество найденых шаров
+                                                       // textBox1.Text = textBox1.Text + "\r\n" + gor[t][0];
+                                                       //textBox1.Text = textBox1.Text + "\r\n" + gor[t][1];
+                                    t++;
+                                    //MessageBox.Show(t.ToString());
+                                }
+                            }
+                        }
+                    }
+                    //собрали видео карты, сортируем  температуры...
+                    if (al[q].ToString().Contains("GPU0 t="))
+                    {
+                        if (t == 1)
+                        {
+                            int a = al[q].ToString().IndexOf("GPU0 t=");
+                            gor[0].Add(al[q].ToString().Substring(a + 7, 2));
+                            //textBox1.Text = textBox1.Text + "\r\n" + al[q].ToString().Substring(a + 7, 2);
+                        }
+                        else
+                        {
+                            string[] st = al[q].ToString().Split(',');
+                            for (int r = 0; r < st.Length; r++)
+                            {
+                                //MessageBox.Show(st[r]);
+                                int a = st[r].ToString().IndexOf("GPU" + r + " t=");
+                                gor[r].Add(st[r].ToString().Substring(a + 7, 2));
+                                //MessageBox.Show(r+" "+st[r].ToString().Substring(a + 7, 2));
+                                //textBox1.Text = textBox1.Text + "\r\n" + r + " " + st[r].ToString().Substring(a + 7, 2);
+
+
+                            }
+                        }
+                    }
+                    if (al[q].ToString().Contains("Total Speed:"))
+                    {
+                        if (t == 1)
+                        {
+                            int a = al[q + 1].ToString().IndexOf("GPU0");
+                            gor[0][2] = gor[0][2] + "|" + al[q + 1].ToString().Substring(a + 5, 6);
+                        }
+                        else
+                        {
+                            string[] st = al[q + 1].ToString().Split(',');
+                            for (int r = 0; r < st.Length; r++)
+                            {
+                                int a = st[r].ToString().IndexOf("GPU" + r);
+                                gor[r][2] = gor[r][2] + "|" + st[r].Substring(a + 5, 6);
+                            }
+                        }
+                    }
+                    if (al[q].ToString().Contains("SHARE FOUND"))
+                    {
+                        if (t == 1)
+                        {
+                            gor[0][3] = (Convert.ToInt16(gor[0][3]) + 1).ToString();
+                        }
+                        else
+                        {
+                            int a = al[q].ToString().IndexOf("GPU");
+                            int m = Convert.ToInt16(al[q].ToString().Substring(a + 4, 2).Trim(')'));
+                            gor[m][3] = (Convert.ToInt16(gor[m][3]) + 1).ToString();
+                            //MessageBox.Show(gor[m][3].ToString());
+                        }
+                    }
+                }
+                //MessageBox.Show("wer");
+                for (int y = 0; y < gor.Count; y++)
+                {
+                    if (y == 0)
+                    {
+                        this.Invoke(new MethodInvoker(() => textBox1.Text = gor[y][0].ToString() + " (" + gor[y][1].ToString() + ")")); }
+                    else
+                    { this.Invoke(new MethodInvoker(() => textBox1.Text = textBox1.Text + "\r\n" + gor[y][0].ToString() + " (" + gor[y][1].ToString() + ")")); }
+                    //считаем среднюю скорость
+                    string[] speed = gor[y][2].ToString().Split('|');
+                    double Sspeed = 0;
+                    for (int p = 1; p < speed.Length; p++)
+                    {
+                        //MessageBox.Show(speed[p]);
+                        try
+                        {
+                            Sspeed = Sspeed + Convert.ToDouble(speed[p].Replace('.', ','));
+                        }
+                        catch
+                        {
+                            //MessageBox.Show(speed[p]); 
+                        }
+                    }
+                    Sspeed = Math.Round(Sspeed / Convert.ToDouble(speed.Length), 3);
+                    //
+                    this.Invoke(new MethodInvoker(() => textBox1.Text = textBox1.Text + "\r\n" + "Средняя скорость = " + Sspeed.ToString()));
+                    //textBox1.Text = textBox1.Text + "\r\n" + "Среднее кол-во шаров = " + Math.Round(Convert.ToDouble(gor[y][3])/ Convert.ToDouble(gor[y].Count - 2) / 2880, 3);
+                    this.Invoke(new MethodInvoker(() => textBox1.Text = textBox1.Text + "\r\n" + "Среднее кол-во шаров = " + Math.Round((Convert.ToDouble(gor[y][3]) / (Convert.ToDouble(gor[y].Count - 2) / 2880)), 3).ToString()));
+                    //MessageBox.Show("1");
+                    //textBox1.Text = textBox1.Text + "\r\n" + gor[i][1].ToString();
+                    gor[y].RemoveRange(0, 4);
+                    gor[y].Sort();
+                    double a = 1;
+                    //textBox1.Text = textBox1.Text + "\r\n" + gor[i][0].ToString();
+                    for (int w = 1; w < gor[y].Count - 1; w++)
+                    {
+
+                        //MessageBox.Show(h.ToString());
+
+                        //MessageBox.Show(gor[i][w - 1].ToString() + " == " + gor[i][w].ToString());
+                        if (gor[y][w].ToString() == gor[y][w - 1].ToString())
+                        {
+                            a++;
+                        }
+                        else
+                        {
+                            //textBox1.Text = textBox1.Text + "\t" + a.ToString() + "\r\n" + gor[i][w].ToString();
+                            //textBox1.Text = textBox1.Text + "\r\n" + gor[i][w].ToString();
+                            double h = Convert.ToDouble(gor[y].Count - 2) / 2880;
+                            //MessageBox.Show(h.ToString());
+                            this.Invoke(new MethodInvoker(() => textBox1.Text = textBox1.Text + "\r\n" + gor[y][w - 1].ToString() + "\t" + Math.Round((a * 30 / 3600) / h, 3).ToString()));
+                            a = 1;
+                        }
+
+
+                    }
+                }
+                this.Invoke(new MethodInvoker(() => textBox1.SelectionStart = textBox1.TextLength));
+                this.Invoke(new MethodInvoker(() => textBox1.ScrollToCaret()));
+                this.Invoke(new MethodInvoker(() => textBox1.Enabled = true));
+            }
+            
+        }
+    }
+}
